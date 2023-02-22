1ce101264aa7c1c47daf61185d0573bf7a5d7e74
Carlos Enrique Oropeza <ceon20020508@gmail.com>
2023-02-22 02:57:14 -0500

    Traducción de página completado

--- app.js
+++ app.js
@@ -60,3 +60,32 @@ const openConfBar = () => {
     let languages = document.querySelector('.languages-bar');
     languages.classList.toggle('open-bar')
 }
+
+//////////////////////////////////////////////////////////////
+
+
+
+const flagsElement = document.getElementById('flags');
+
+const textsToChange = document.querySelectorAll('[data-section]')
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`)
+    const texts = await requestJson.json()
+
+    for(const textToChange of textsToChange) {
+        const section = textToChange.dataset.section
+        const value = textToChange.dataset.value
+        const subvalue = textToChange.dataset.subvalue
+
+        if(subvalue) {
+            textToChange.innerHTML = texts[section][value][subvalue]
+        } else {
+            textToChange.innerHTML = texts[section][value]
+        }
+    }
+}
+
+flagsElement.addEventListener('click', (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+})
